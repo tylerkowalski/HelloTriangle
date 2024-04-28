a54a48ddcfc556603763a099364c64c691c8a7e6
tkowalski9938 <tkowalski9938@gmail.com>
2024-04-28 12:14:04 -0400

    dynamic viewport+scissor

--- app.cpp
+++ app.cpp
@@ -38,8 +38,9 @@ void App::createPipelineLayout() {
 }
 
 void App::createPipeline() {
-  auto pipelineConfig = HtPipeline::defaultPipelineConfigInfo(
-      htSwapChain->width(), htSwapChain->height());
+  PipelineConfigInfo pipelineConfig{};
+  HtPipeline::defaultPipelineConfigInfo(pipelineConfig);
+
   pipelineConfig.renderPass = htSwapChain->getRenderPass();
   pipelineConfig.pipelineLayout = pipelineLayout;
   htPipeline = std::make_unique<HtPipeline>(
@@ -100,6 +101,18 @@ void App::recordCommandBuffer(int imageIndex) {
 
   vkCmdBeginRenderPass(commandBuffers[imageIndex], &renderPassInfo,
                        VK_SUBPASS_CONTENTS_INLINE);
+
+  VkViewport viewport{};
+  viewport.x = 0.0f;
+  viewport.y = 0.0f;
+  viewport.width = static_cast<float>(htSwapChain->getSwapChainExtent().width);
+  viewport.height =
+      static_cast<float>(htSwapChain->getSwapChainExtent().height);
+  viewport.minDepth = 0.0f;
+  viewport.maxDepth = 0.0f;
+  VkRect2D scissor{{0, 0}, htSwapChain->getSwapChainExtent()};
+  vkCmdSetViewport(commandBuffers[imageIndex], 0, 1, &viewport);
+  vkCmdSetScissor(commandBuffers[imageIndex], 0, 1, &scissor);
 
   htPipeline->bind(commandBuffers[imageIndex]);
   // htModel->bind(commandBuffers[i]);
--- ht_pipeline.hpp
+++ ht_pipeline.hpp
@@ -7,17 +7,21 @@
 
 namespace ht {
 struct PipelineConfigInfo {
-  VkViewport viewport;
-  VkRect2D scissor;
+  VkPipelineViewportStateCreateInfo viewportInfo;
   VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
   VkPipelineRasterizationStateCreateInfo rasterizationInfo;
   VkPipelineMultisampleStateCreateInfo multisampleInfo;
   VkPipelineColorBlendAttachmentState colorBlendAttachment;
   VkPipelineColorBlendStateCreateInfo colorBlendInfo;
   VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+  std::vector<VkDynamicState> dynamicStateEnables;
+  VkPipelineDynamicStateCreateInfo dynamicStateInfo;
   VkPipelineLayout pipelineLayout = nullptr;
   VkRenderPass renderPass = nullptr;
   uint32_t subpass = 0;
+
+  PipelineConfigInfo(const PipelineConfigInfo &) = delete;
+  PipelineConfigInfo &operator=(const PipelineConfigInfo &) = delete;
 };
 
 class HtPipeline {
@@ -32,8 +36,7 @@ public:
 
   void bind(VkCommandBuffer commandBuffer);
 
-  static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width,
-                                                      uint32_t height);
+  static void defaultPipelineConfigInfo(PipelineConfigInfo &configInfo);
 
 private:
   static std::vector<char> readFile(const std::string &filePath);
